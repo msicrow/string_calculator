e85af2b9b5ff66a5e08dcc8845650da5e70b9b2e
Michael <michaelscrow@gmail.com>
2020-11-22 14:50:32 +0000

    Added implementation of shunting yard algorithm

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -1,50 +1,149 @@
-def help_command():
-    print("Please enter either an operation using either addition or subtraction.")
+import re
+import functools
+
+commands = {"/help": "Please enter a calculation. "
+                     "Expressions can include the following operations:"
+                     "+, -, *, / and brackets can be used.",
+            "/exit": "Bye!"}
+user_variables = {}
 
 
-def standard_calc(string_inp):
-    if "-" in string_inp or "+" in string_inp:
-        if string_inp.endswith("-") or string_inp.endswith("+"):
-            print("Invalid expression")
-        else:
-            operation = string_inp.replace("-", "+-")
-            operation = operation.replace(" ", "").split("+")
-            print(sum([int(x) for x in operation if x != ""]))
+def compare(key_1, key_2):
+    if len(key_1) > len(key_2):
+        return 1
+    elif len(key_2) > len(key_1):
+        return -1
     else:
-        if string_inp.isdigit():
-            print(string_inp)
+        return 0
+
+
+def replace_user_variable(inp):
+    key_list = [key for key in user_variables.keys()]
+    key_list.sort(key=functools.cmp_to_key(compare), reverse=True)
+    for key in key_list:
+        if key in inp:
+            inp = inp.replace(key, user_variables[key])
         else:
-            if "-" not in string_inp or "+" not in string_inp:
-                print("Invalid expression")
-            else:
-                operation = string_inp.replace(" ", "")
-                print(sum([int(x) for x in operation if x != ""]))
+            pass
+    return inp
 
 
-def double_minus(string_inp):
-    if string_inp.endswith("--"):
-        print("Invalid expression")
-    else:
-        operation = string_inp.replace("--", "+")
-        standard_calc(operation)
-
-
-while True:
-    calc = input()
-    if not calc:
-        continue
-    elif calc.startswith("/"):
-        if calc == "/exit":
-            print("Bye!")
+def format_string(inp):
+    inp = inp.replace(" ", "")
+    while True:
+        length = len(inp)
+        inp = inp.replace("--", "+").replace("++", "+").replace("-+", "-").replace("+-", "-")
+        if len(inp) == length:
             break
-        elif calc == "/help":
-            help_command()
         else:
-            print("Unknown command")
-    elif "--" in calc:
-        double_minus(calc)
+            continue
+    inp = replace_user_variable(inp)
+    return inp
+
+
+def peek(stack):
+    return stack[-1] if stack else None
+
+
+def operation_precedence(op1, op2):
+    precedence = {"+": 1, "-": 1, "*": 2, "/": 2}
+    return precedence[op1] >= precedence[op2]
+
+
+def calculation(nums, ops):
+    operator = ops.pop()
+    lhs = nums.pop()
+    rhs = nums.pop()
+    if operator == "+":
+        nums.append(lhs + rhs)
+    elif operator == "-":
+        nums.append(rhs - lhs)
+    elif operator == "*":
+        nums.append(lhs * rhs)
+    elif operator == "/":
+        nums.append(rhs / lhs)
+
+
+def store_var(inp):
+    variable = inp.replace(" ", "").split("=")
+    if re.search(r"\d", variable[0]):
+        print("Invalid identifier")
+    elif re.search(r"(?:\d+[a-zA-Z]+|[a-zA-Z]+\d+)", variable[1]) \
+            or (variable[1].isalpha() and variable[1] not in user_variables.keys()):
+        print("Invalid assignment")
     else:
-        try:
-            standard_calc(calc)
-        except ValueError:
-            print("Invalid expression")
+        variable[1] = replace_user_variable(variable[1])
+        operator = re.findall("[+/*()-]", variable[1])
+        if operator:
+            variable[1] = str(evaluate(variable[1]))
+        user_variables[variable[0]] = variable[1]  # if no operator, key == LHS, value == RHS
+
+
+def evaluate(inp):
+    tokens = re.findall("[+/*()-]|\d+", inp)
+    integers = []
+    operators = []
+    try:
+        for token in tokens:
+            if token.isnumeric():
+                integers.append(int(token))
+            elif token == "(":
+                operators.append(token)
+            elif token == ")":
+                stack_top = peek(operators)
+                while stack_top and stack_top != "(":
+                    calculation(integers, operators)
+                    stack_top = peek(operators)
+                operators.pop()
+            else:
+                stack_top = peek(operators)
+                while stack_top is not None and stack_top not in "()" and operation_precedence(stack_top, token):
+                    calculation(integers, operators)
+                    stack_top = peek(operators)
+                operators.append(token)
+
+        while peek(operators) is not None:
+            calculation(integers, operators)
+        return integers[0]
+    except IndexError:
+        print("Invalid expression")
+
+
+def main():
+    while True:
+        inp = input()
+        if inp in commands:
+            if inp == "/help":
+                print(commands["/help"])
+            else:
+                print(commands["/exit"])
+                break
+        elif not inp:
+            pass
+        elif inp[:1] == "/":
+            print("Unknown command")
+        elif "=" in inp:
+            if inp.count("=") > 1:
+                print("Invalid assignment")
+            else:
+                store_var(inp)
+        elif inp.startswith("-") and not re.match("[+/*()-]", inp[1:]):
+            print(inp)
+        elif re.search(r"(?:\d+[a-zA-Z]+|[a-zA-Z]+\d+)", inp):
+            print("Invalid identifier")
+        elif inp.isalpha():
+            if inp in user_variables.keys():
+                print(user_variables[inp])
+            else:
+                print("Unknown variable")
+        else:
+            inp = format_string(inp)
+            if evaluate(inp) is not None:
+                print(evaluate(inp))
+            else:
+                pass
+
+
+if __name__ == "__main__":
+
+    main()
